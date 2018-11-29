d1539d105d8019c34c5a85451d78f2125471e134
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-28 22:25:59 -0200

    Add project files.

---
+++ ExemploStorage/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ExemploStorage
+{
+    public class Aluno : TableEntity
+    {
+        public Aluno(string rm, string cidade) : base(cidade, rm)
+        {
+
+        }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ExemploStorage/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ExemploStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inclusão da Access Key string do Azure
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
+
+            #region blob
+            //var blobClient = account.CreateCloudBlobClient();
+
+            //var container = blobClient.GetContainerReference("rm331334");
+            //container.CreateIfNotExistsAsync().Wait();
+
+            //var blob = container.GetBlockBlobReference("jsbarros2.txt");
+            //blob.UploadTextAsync("Vai Corinthians").Wait();
+            //var sas = blob.GetSharedAccessSignature(new SharedAccessBlobPolicy
+            //{
+            //    Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write,
+            //    SharedAccessExpiryTime = DateTime.Now.AddMinutes(10)
+            //});
+
+            //Console.WriteLine($"{blob.Uri}{sas}");
+            #endregion
+
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("rm331334");
+            table.CreateIfNotExistsAsync().Wait();
+            var entity = new Aluno("331334", "SP")
+            {
+                Email = "331334@fiap.com",
+                Nome = "Jefferson"
+            };
+
+            table.ExecuteAsync(TableOperation.Insert(entity));
+
+            Console.Read();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
